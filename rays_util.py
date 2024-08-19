fca7a34589f430987dd689426351cfafa9b33e46
dayoxiao <daanyoyo@gmail.com>
2024-08-19 16:13:32 +0800

    llff update

--- rays_util.py
+++ rays_util.py
@@ -26,3 +26,22 @@ def get_rays(
   # Origin
   rays_o = c2w[:3, -1].expand(rays_d.shape)
   return rays_o, rays_d
+
+def ndc_rays(H, W, focal, near, rays_o, rays_d):
+    # shift ray origins to near plane
+    t = -(near + rays_o[..., 2]) / rays_d[...,2]
+    rays_o = rays_o + t[...,None] * rays_d
+    
+    # projection
+    o0 = -1. / (W / (2. * focal)) * rays_o[...,0] / rays_o[...,2]
+    o1 = -1. / (H / (2. * focal)) * rays_o[...,1] / rays_o[...,2]
+    o2 =  1. +  2. * near / rays_o[...,2]
+
+    d0 = -1. / (W / (2. * focal)) * (rays_d[...,0]/rays_d[...,2] - rays_o[...,0]/rays_o[...,2])
+    d1 = -1. / (H / (2. * focal)) * (rays_d[...,1]/rays_d[...,2] - rays_o[...,1]/rays_o[...,2])
+    d2 = -2. * near / rays_o[...,2]
+    
+    rays_o = torch.stack([o0,o1,o2], -1)
+    rays_d = torch.stack([d0,d1,d2], -1)
+    
+    return rays_o, rays_d
